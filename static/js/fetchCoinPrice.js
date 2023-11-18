bac7f4e6210f68413900041175a907cd12424c35
Giuliano <sistemas@paracan.com.py>
2023-11-18 10:55:18 -0300

    Format changes

--- static/js/fetchCoinPrice.js
+++ static/js/fetchCoinPrice.js
@@ -15,14 +15,15 @@ function fetchCoinsPrice() {
       data.forEach((coin) => {
         console.log(coin);
         const price = coin.price.toFixed(2);
+        const formattedPrice = parseFloat(price).toLocaleString('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
         content += `
                 <div class="coin-price-container">
                     <div class="coin-price-coin">
                       <i class="fa-solid fa-${coin.name.toLowerCase()}-sign"></i>
                     </div>
-                    <div class="coin-price-coin">${coin.coin}</div>
                     <div class="coin-price-name">${coin.name}</div>
-                    <div class="coin-price-price">${price}</div>
+                    <div class="coin-price-coin">${coin.coin}</div>
+                    <div class="coin-price-price">$${formattedPrice}</div>
                 </div>
             `;
       });
